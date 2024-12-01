69db9d629bbcd4658957077fbbd85b841ae7fbe5
cheeseballDev <cheeseballbusinessemail@gmail.com>
2024-12-01 19:00:44 +0800

    Added inheritance from ChatbotData to Chatbot

--- Chatbot/Chatbot.java
+++ Chatbot/Chatbot.java
@@ -1,5 +1,16 @@
 package FinalsProject.Chatbot;
 
-public class Chatbot {
-    
+public class Chatbot extends ChatbotData implements Runnable {
+
+    public void runChatbot() {
+
+    }
+
+    public void run() {
+        try {
+            Thread.sleep(50);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
