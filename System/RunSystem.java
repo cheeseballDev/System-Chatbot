da63f1872de4667436bab8b20a9da06be639594a
cheeseballDev <cheeseballbusinessemail@gmail.com>
2024-12-01 18:53:45 +0800

    Removed system file due to it no longer being a requirement

--- System/RunSystem.java
+++
@@ -1,8 +0,0 @@
-package FinalsProject.System;
-
-public class RunSystem {
-    public static void main(String[] args) {
-        MainSystem system = new MainSystem();
-        system.initializeSystem();
-    }
-}
