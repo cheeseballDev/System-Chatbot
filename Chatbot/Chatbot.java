be4ac3186380c5413cef3c6ad8a163511fcdbc30
cheeseballDev <cheeseballbusinessemail@gmail.com>
2024-12-01 18:55:38 +0800

    Added one file named ChatbotData where it contains all the chatbot's data

--- Chatbot/Chatbot.java
+++ Chatbot/Chatbot.java
@@ -1,21 +1,5 @@
 package FinalsProject.Chatbot;
 
-import java.util.HashMap;
-
 public class Chatbot {
-    private HashMap<String, String> responses;
     
-    public Chatbot() {
-        responses = new HashMap<>();
-        loadResponses();
-    }
-
-    private void loadResponses() {
-        responses.put("hello", "Hi. I am BOT Mika, How may I help you?");
-        responses.put("hi", "Hello. I am BOT Mika, How may I help you?");
-    }
-
-    public String getResponse(String userResponse) {
-        return responses.getOrDefault(userResponse.toLowerCase().trim(), "I did not understand what you said.");
-    }
 }
